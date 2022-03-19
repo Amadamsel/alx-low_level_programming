955ba74aac27370d4d2e42f0540178be4dcdb9fc
Amadamsel <amabeescouture@gmail.com>
2022-03-19 03:54:23 +0100

    It was not that bad

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,28 +1,21 @@
-
-#include <stdio.h>
-
+[D[D#include <stdio.h>
 /**
  * main - causes an infinite loop
- * Return: 0
- [A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-[C[C[C[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[
+ *
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
-	printf("Infinite loop incoming :(\n");[A[A[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+[D[D[D[D[D[D[Dint i;
+printf("Infinite loop incoming :(\n");
+i = 0;
 
-[A[3~[3~[3~[H[3~[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-	printf("Infinite loop incoming :(\n");
-
-[D[D[D[D[D[Di = 0;
-
-[D[D[D[D[D[D[D[D/*
- *while (i < 10)
+/*
+ *while (1 < 10)
  *{
- *Infinite loop - No increase of variable
+ *putchar(i);
+[A[C[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[CInfinite loop - No increase of variable
  *putchar(i);
 }
 */
@@ -30,3 +23,5 @@ int main(void)
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
+[A[C[3~
+:
