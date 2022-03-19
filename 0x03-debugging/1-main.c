50ec2726763582276b2597423d19873081838753
Amadamsel <amabeescouture@gmail.com>
2022-03-19 03:38:12 +0100

    My commit message

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,20 +1,28 @@
+
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
- *
+ * Return: 0
+ [A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[C[C[C[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
+	printf("Infinite loop incoming :(\n");[A[A[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+
+[A[3~[3~[3~[H[3~[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
+[D[D[D[D[D[Di = 0;
 
-/*
+[D[D[D[D[D[D[D[D/*
  *while (i < 10)
  *{
- *Infinite loop - No increase of variables
+ *Infinite loop - No increase of variable
  *putchar(i);
 }
 */
@@ -22,4 +30,3 @@ int main(void)
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
-
