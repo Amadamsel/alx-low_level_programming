e5bede1f0b6c46a006ac9b86e1f879dc0f312075
Amadamsel <amabeescouture@gmail.com>
2022-03-20 23:09:14 -0700

    My commit for 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,12 +4,13 @@
  *
  * Return: 0
  */
-
 int main(void)
 {
-	int i;
-	printf("Infinite loop incoming :(\n");
-	i = 0;
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
 
 /*
 *while (i < 10)
@@ -18,8 +19,6 @@ int main(void)
 *putchar(i);
 }
 */
-
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
-
