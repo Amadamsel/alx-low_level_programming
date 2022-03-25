bd6d8d4f15119d3a3a088084dc116ec3faea321c
Amadamsel <amabeescouture@gmail.com>
2022-03-25 04:59:23 -0700

    My commit message for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: zero always
+ */
+int main(void)
+{
+
+int i;
+
+for (q = 0; q < 10; q++)
+{
+printf('%q', q);
+}
+
+print("\n");
+
+return(0);
+
+}
