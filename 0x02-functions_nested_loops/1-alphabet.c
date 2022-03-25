26a2c1f7f749c7388ce833bf35250c3a38409457
Amadamsel <amabeescouture@gmail.com>
2022-03-25 14:12:34 -0700

    My commit message for task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints the letters of the alphabet in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+
+{
+char y;
+
+for (y = 'a'; y <= 'z'; y++)
+{
+_putchar(y);
+}
+
+_putchar ('\n');
+
+return (0);
+
+}
+
