a24ab4366b876951baf16c2ca82e761b4659c6d1
amabeescouture@gmail.com <amabeescouture@gmail.com>
2022-06-10 11:50:40 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/felix.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - testing code
+ * Return: Always 0
+ */
+
+int main()
+{
+	int a = 10;
+	int b = 15;
+
+	int sum = a + b;
+	printf("The sum is: %d\n",sum);
+	felix();
+	return (0);
+}
+
+
+void felix(void)
+{
+	// something will be here
+}
