eabd61ddf0659d9dbb75972477b004c1364cb04e
amabeescouture@gmail.com <amabeescouture@gmail.com>
2022-06-10 06:53:53 -0700

    Modification to _isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * _isupper - checks for uppercase
  * @c: integer parameter
  * Return: Always 0
  */
+
 int _isupper(int c)
 {
-    return (c >= 'A' && c <= 'Z');
+	return (c >= 'A' && c <= 'Z');
 }
