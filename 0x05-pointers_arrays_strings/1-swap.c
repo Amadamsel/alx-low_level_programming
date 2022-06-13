dd708ce82d77dda6e72f7267d7e6ad1de812d3cc
amabeescouture@gmail.com <amabeescouture@gmail.com>
2022-06-13 09:39:18 -0700

    My commit for task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - does swapping of memory contents
+ * @g: parameter
+ * @b: parameter
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *g, int *b)
+{
+  int x = *a;
+  *a = *b;
+  *b = x;
+}
