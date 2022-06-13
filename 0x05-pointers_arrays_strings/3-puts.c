cbb027736f169cb7738eb9fe2838866b65c23214
amabeescouture@gmail.com <amabeescouture@gmail.com>
2022-06-13 10:30:25 -0700

    My commit for task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: parameter
+ *
+ * Return: Always 0 success
+ */
+
+void _puts(char *str);
+{
+	puts(str);
+}
